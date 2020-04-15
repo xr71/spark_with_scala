8f1496df6f7bb98e8bbccc3f4c291f095bf9236b
Xu Ren <musicalxu@hotmail.com>
2020-04-16 05:05:10 +0800

    scala course init

---
+++ Conditionals0.scala
@@ -0,0 +1,21 @@
+if (true) {
+  println("this is true")
+}
+val x = "zzz"
+
+if (x.endsWith("o")) {
+  println("this ends with an o")
+} else {
+  println("this does not end with an o")
+
+}
+
+
+val name = "George"
+if (name == "Sammy") {
+  println("Your name is sammy")
+} else if (name == "George") {
+  println("Welcome George")
+}
+
+
---
+++ HelloWorld.scala
@@ -0,0 +1,12 @@
+println("Hello World")
+
+val x = "this is a string"
+var ss = "this is a mutable string"
+
+println(x contains "string")
+// x = "this won't be allowed"
+ss = "this will be allowed"
+
+if (3 == 3) {
+  println("3 is equal to 3!")
+}
